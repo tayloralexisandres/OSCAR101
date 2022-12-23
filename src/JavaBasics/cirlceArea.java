0e47393b9b613cdcba40e387e7d14defc30dfc69
taylo <tayloralexisandres@gmail.com>
2022-12-22 18:06:02 -0700

    First two basic java tasks

---
+++ src/JavaBasics/cirlceArea.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+
+public class cirlceArea {
+    public static void main(String[] args) {
+
+        double radius=4;
+        double PI=3.14;
+        double area= radius*2*PI;
+
+        System.out.println("area of circle is= "+area);
+
+
+
+    }
+}
---
+++ src/JavaBasics/swapInteger.java
@@ -0,0 +1,19 @@
+package JavaBasics;
+
+public class swapInteger {
+    public static void main(String[] args) {
+
+        int a= 5;
+        int b= 10;
+        int c=0;
+
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("a = "+a+" b= "+ b);
+
+
+        // this java class was created in the working directory
+
+    }
+}
