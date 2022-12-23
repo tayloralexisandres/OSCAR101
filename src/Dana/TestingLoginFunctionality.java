6adea3508eb5834f5c17b8cafb3b60526bf2cf0d
usdanakl <usdanakl@gmail.com>
2022-12-22 19:48:41 -0600

    Changes made

--- src/Dana/TestingLoginFunctionality.java
+++ src/Dana/TestingLoginFunctionality.java
@@ -2,7 +2,7 @@ package Dana;
 
 public class TestingLoginFunctionality {
     public static void main(String[] args) {
-        System.out.println("I am testing");
+        System.out.println("I am testing login functional");
     }
 
 
